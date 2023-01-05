a73aa9b8ab5b97e4f566b6e4559ad54eae3c26bd
Grant Crawford <grantttt.crawford@gmail.com>
2023-01-04 22:18:18 -0600

    Exercise 1 part 4 fixed

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -1,6 +1,6 @@
 package exercises;
 
-
+import java.util.Scanner;
 public class Alice {
     public static void main(String[] args){
         String sentence = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once" +
@@ -8,7 +8,14 @@ public class Alice {
                 "her sister was reading, but it had no pictures or conversations in it," +
                 "'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
         sentence = sentence.toLowerCase();
-        boolean sentenceCheck = sentence.contains("alice");
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a term to search for: ");
+        String userValue = input.nextLine();
+        userValue = userValue.toLowerCase();
+        boolean sentenceCheck = sentence.contains(userValue);
         System.out.println(sentenceCheck);
+
     }
 }
